92e14759825bad08e2d8ee38338067c78ba979b1
pjf <pjf@6613b302-cbcb-4f93-ba3e-73d4af713c8e>
2008-05-23 13:37:53 +0000

    Tightened

--- v0.1/lib/IPC/SRLock/Sysv.pm
+++ v0.1/lib/IPC/SRLock/Sysv.pm
@@ -188,6 +188,85 @@ sub _set {
 
 __END__
 
+=pod
+
+=head1 Name
+
+IPC::SRLock::Sysv - Set/reset locks using semop and shmop
+
+=head1 Version
+
+0.1.$Revision$
+
+=head1 Synopsis
+
+   use IPC::SRLock;
+
+   my $config   = { tempdir => q(path_to_tmp_directory), type => q(sysv) };
+
+   my $lock_obj = IPC::SRLock->new( $config );
+
+=head1 Description
+
+Uses System V semaphores to lock access to a shared memory file
+
+=head1 Subroutines/Methods
+
+=head2 _init
+
+=head2 _get_semid
+
+=head2 _get_shmid
+
+=head2 _list
+
+=head2 _reset
+
+=head2 _set
+
+=head1 Diagnostics
+
+=head1 Configuration and Environment
+
+=head1 Dependencies
+
+=over 4
+
+=item L<IPC::SRLock>
+
+=item L<IPC::SysV>
+
+=item L<Readonly>
+
+=back
+
+=head1 Incompatibilities
+
+There are no known incompatibilities in this module
+
+=head1 Bugs and Limitations
+
+There are no known bugs in this module.
+Please report problems to the address below.
+Patches are welcome
+
+=head1 Author
+
+Peter Flanigan, C<< <Support at RoxSoft.co.uk> >>
+
+=head1 License and Copyright
+
+Copyright (c) 2008 Peter Flanigan. All rights reserved
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself. See L<perlartistic>
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE
+
+=cut
+
 # Local Variables:
 # mode: perl
 # tab-width: 3
